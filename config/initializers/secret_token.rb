3780ddce01650568f331ef612d50bb12b154620e
jose pedraza <josepedraza@jose-pedrazas-MacBook.local>
2011-03-30 16:29:23 -0500

    initial commit for upload the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saicapellanes::Application.config.secret_token = '45c58e4d7590c57b2862e475b7674abd9a398894b3afcec4c2beb8fe2f5882292e9d4c01d1fd13730ba291fbcc7ff926d628d427031416415b748f034e978e33'
