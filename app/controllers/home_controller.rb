fef76a72c5503f30b9ae02e98478f63f5a08ed36
jose pedraza <josepedraza@jose-pedrazas-MacBook.local>
2011-04-04 23:05:28 -0500

    adding application layout

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,29 @@
+
+class HomeController < ApplicationController
+  
+#Controlador que administra las acciones de las paginas del frontend del portal
+  
+  def index
+  end
+  
+  def about_us
+  end
+  
+  def more_info
+  end
+  
+  def calendar
+  end
+  
+  def contact
+  end
+  
+  def gallery
+  end
+  
+  def our_invitations
+  end
+  
+
+  
+end
---
+++ config/initializers/smtp_settings.rb
@@ -0,0 +1,10 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address        => 'smtp.gmail.com',
+    :port           => 587,
+    :domain         => 'saicapellanes.biz',
+    :authentication => :plain,
+    :user_name      => 'pedrazageek@gmail.com',
+    :password       => '33923777'
+  }
