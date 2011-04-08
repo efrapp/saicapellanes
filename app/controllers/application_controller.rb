92cbd13bd41df8443fd7f6d9a04e63760b616e71
fbruges <fbruges@gmail.com>
2011-04-07 22:42:08 -0500

    redirect after sign in to members/index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+  
+  def after_sign_in_path_for(resource_or_scope)
+    members_index_path
+  end
+  
 end
