ae735ddb9a0f1e84a1f9a6e0a771fb2d05fc571d
pedrazaJose <pedrazageek@gmail.com>
2011-04-09 04:34:46 -0500

    implementing the mailer for cards product type

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,7 @@
+class EmailsController < ApplicationController
+  def cards_mail
+    message = {:personal_data=>{:name=>params[:name], :last_name=>params[:last_name], :gender=>params[:gender],:parent_name=>params[:parent_name], :document_type=>params[:document_type],:document_number=>params[:document_number], :country=>params[:country], :state=>params[:state]},:contact_information=>{:adress=>params[:adress], :zip_code=>params[:zip_code],:city=>params[:city], :state=>params[:state], :cell_phone=>params[:cell_phone], :email=>params[:email]}, :vendor_information=>{:promo_code=>params[:promo_code], :comment=>params[:comment], :newsletters=>params[:newsletters]}}
+    Emailer.deliver_cards("pedrazaj_24@hotmail.com", "Pedido Tarjeta", message);
+    redirect_to members_index_path
+  end
+end
---
+++ app/mailers/emailer.rb
@@ -0,0 +1,12 @@
+class Emailer < ActionMailer::Base
+  def cards(recipient, subject, message, sent_at = Time.now)
+     @subject = subject
+     @recipients = recipient
+     @from = 'no-reply@saicapellanes.biz'
+     @sent_on = sent_at
+	  @body["title"] = 'This is title'
+ 	  @body["email"] = 'sender@yourdomain.com'
+  	@body["message"] = message
+    @headers = {}
+  end
+end
